fde5686e59b4973d159597f5dfe81469b30ea152
Samuel Martinez <sma.2904@gmail.com>
2021-12-14 15:44:40 -0600

    proyecto finalizado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const url = 'https://platzi-avo.vercel.app';
+const api = 'https://platzi-avo.vercel.app/api/avo';
+const rootRender = document.querySelector('#root')
 
-console.log('Happy hacking :)')
+const fetchData = async() =>{
+    const resp = await fetch(api);
+    const { data } = await resp.json();  
+    data.map(el =>{
+        const container = document.createElement('div');
+        const {image, price, name, id} = el;
+        const html = `
+        <img 
+        class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6" 
+        src="${url}${image}" 
+        alt="${id}">
+        <div class="text-center md:text-left">
+        <h2 class="text-lg">${name}</h2>
+        <p class"text-gray-600">Price:${price}</p>
+        </div>
+        <hr/>
+        `;
+        container.innerHTML = html;
+        rootRender.append(container);
+        rootRender.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300";
+        return container;
+    });
+} 
+
+fetchData();
